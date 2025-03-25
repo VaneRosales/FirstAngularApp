2e27925c2066f8cb91b1301123131c7601f6a964
VaneRosales <jvanessa.rr@gmail.com>
2025-03-25 09:59:50 -0600

    Leccion 3: Interfaz HousingLocation

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
 @Component({
   selector: 'app-housing-location',
   standalone: true,
